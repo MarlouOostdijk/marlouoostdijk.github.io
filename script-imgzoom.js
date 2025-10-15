bc4d34306d2e04d5ad8efeb133436251361ad59a
MarlouOostdijk <marlou.oostdijk@outlook.com>
2025-10-15 20:56:50 +0200

    Marlou Oostdijk - Portfolio [Version 2.5]

---
+++ script-imgzoom.js
@@ -0,0 +1,14 @@
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = lightbox.querySelector('img');
+
+document.querySelectorAll('.gallery img').forEach(img => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src;
+        lightbox.style.display = 'flex';
+    });
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const nav = document.querySelector("header nav");
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".contact-form");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault(); // voorkomt dat de pagina herlaadt
+
+    const formData = new FormData(form);
+
+    const response = await fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: { 'Accept': 'application/json' }
+    });
+
+    if (response.ok) {
+      alert("Bedankt voor je bericht! 😊");
+      form.reset(); // maakt het formulier leeg
+    } else {
+      alert("Oeps! Er ging iets mis bij het verzenden. Probeer het opnieuw.");
+    }
+  });
+});
